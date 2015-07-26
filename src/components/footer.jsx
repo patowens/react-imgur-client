855bb4eb2229f2f0fd8384f74dfc5c582d2122bc
Pat Owens <pwns@pats-macbook.iinet>
2015-07-26 23:43:11 +1000

    imgur client in react

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+		return <div className="footer">
+		<hr />
+		<h6>A quick Imgur client using React + Reflux</h6>
+		</div>
+	}
+})
